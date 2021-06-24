55c10aebc50d00e5297ccbccb5c6612d63da846a
Jose19-e <72581134+Jose19-e@users.noreply.github.com>
2021-06-23 19:04:42 -0500

    Add files via upload

---
+++ tarea6/Tarea6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea6;
+
+/**
+ *
+ * @author jose_
+ */
+public class Tarea6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Alumno alum1 = new Alumno("318088378", "ingenieria en Computación", 2021, "José", 19, 1, 1.79f);
+        System.out.println(alum1.toString());
+    }
+    
+}
